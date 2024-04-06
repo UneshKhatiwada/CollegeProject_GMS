ddc90a4bc825dd292e193d6b16c5b51bf3012416
Subarna Khatiwoda <subarna.khatiwada13@gmail.com>
2024-04-06 23:13:29 +0545

    College Project

---
+++ gym/header1.php
@@ -0,0 +1,32 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vyayamlaya</title>
+    <!-- Font Awesome -->
+    <link href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" rel="stylesheet" />
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
+    <!-- MDB -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.2.0/mdb.min.css" rel="stylesheet" />
+
+    <style>
+        .btn {
+            margin: 0;
+        }
+
+        #btn1{
+            background: #428f9d;
+            
+        }
+
+        #btn1:hover{
+            background: #30c1db;
+        }
+    </style>
+</head>
+<body>
+    
